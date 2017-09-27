fa42caf56a0e68195bf583fa90efa9993696cc13
wkylin.w <wkylin.w@gmail.com>
2017-09-28 00:22:17 +0800

    HttpClient

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+declare let $: any;
 
 @Component({
     selector: 'app-login',
@@ -7,21 +9,41 @@ import { HttpClient } from '@angular/common/http';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    captchaCode: String;
+    captchaCode = '';
+    api = 'http://47.52.143.236/api';
     constructor(private http: HttpClient) {
     }
     getCode() {
-        console.log('get code');
-    }
-    ngOnInit() {
-        this.http.get('/captcha/getCaptchaCode').subscribe(
+        this.http.get(`${this.api}/captcha/getCaptchaCode`, {responseType: 'text'}).subscribe(
+        // this.http.post('https://wz.91yaowang.com/app/webservice/v2/member/getCaptcha?timer=' + new Date().getTime(), {}).subscribe(
             data => {
-                console.log('get captcha code ');
-                this.captchaCode = data['results'];
+                this.captchaCode = 'data:image/png;base64,' + data['data'].captcha;
             },
             err => {
-                console.log(err['error']);
+                console.log(err['error']['text']);
+                this.captchaCode = err['error']['text'];
             }
         );
+    }
+    login() {
+        this.http.post(`${this.api}/Users/login`, {
+                'username': $('.login-name').val(),
+                'password': $('.login-pwd').val(),
+                'vcode': $('.login-vcode').val()
+            }, {
+                headers: new HttpHeaders().set('HTTP_USER_AGENT', 'other'),
+            })
+            .subscribe(
+                data => {
+                    console.log(data);
+                },
+                err => {
+                    console.log(err['error']);
+                }
+            );
+    }
+    ngOnInit() {
+        // this.login();
+        // this.getCode();
     }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare let $: any;
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    api = 'http://47.52.143.236/api/';
+    constructor(private http: HttpClient) {
+    }
+    onRegister() {
+        this.http.post( `${this.api}Users/register`, {
+            'bankPwd': '',
+            'defLoginPwd': '',
+            'username': $('.input-name').val(),
+            'password': $('.input-pwd').val(),
+            'rePassword': $('.input-re-pwd').val(),
+            'qq': $('.input-qq').val(),
+            'vcode': $('.input-vcode').val(),
+            'code': '',
+            'user_id': ''
+        }).subscribe(
+            data => {
+                console.log(data);
+            },
+            err => {
+                console.log(err['error']['text']);
+            }
+        );
+    }
+    ngOnInit() {
+    }
 }
